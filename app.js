de6db0cb1275eeeba4cb7c54ac4f9839bab2c038
Fernando Argueta <avismael.argueta@gmail.com>
2026-05-27 11:27:17 -0600

    fix: increase speaking duration from 30 to 60 seconds

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
   const STORAGE_KEY = "spellingBeeInseMistakes";
   const VOICE_SETTINGS_KEY = "spellingBeeInseVoiceSettings";
   const PREFERRED_VOICE_NAMES = ["natural", "online", "neural", "google", "microsoft", "zira", "aria", "jenny", "samantha", "daniel"];
-  const SPEAKING_SECONDS = 30;
+  const SPEAKING_SECONDS = 60;
   const COMPETITION_SECONDS = 60;
   const DEFAULT_TEAM_KEYS = ["a", "s", "d", "f", "j", "k", "l", ";"];
   const COMPETITION_SHORTCUTS = {
